f2ca887b917e26897e0087bb80461b9bacfa1066
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-11-16 02:10:45 -0300

    Aula06-State

--- first-project/src/components/Form.js
+++ first-project/src/components/Form.js
@@ -1,9 +1,16 @@
+import { useState } from 'react'
+
 function Form(){
 
     function cadastrarUsuario(e){
         e.preventDefault(); //impede o envio de requisição (Impede a página de recarregar)
-        console.log("Cadastrou o Usuario")
+        console.log(name)
+       
+        console.log(`Usuário ${name} foi cadastrado com a senha ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return(
         <div>
@@ -11,14 +18,25 @@ function Form(){
             <form onSubmit={cadastrarUsuario}>
                 <div>
                     <label htmlFor="name">Login:</label>
-                    <input type="text" id="name" placeholder="Digite se nome" />
+                    <input 
+                    type="text" 
+                    id="name" 
+                    placeholder="Digite se nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} 
+                    />
                 </div>
                 <div>
                     <label htmlFor="password">Senha:</label>
-                    <input type="password" id="password" placeholder="Digite se nome" />
+                    <input type="password" id="password" placeholder="Digite se nome" 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    />
                 </div>
                 <div>
-                    <input type="submit" value="Cadastrar" />
+                    <input 
+                    type="submit" 
+                    value="Cadastrar" 
+                    />
                 </div>
                 
             </form>
