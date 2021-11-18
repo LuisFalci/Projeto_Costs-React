c657f4b878537f44924d52fa198e574b2039dd37
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-11-18 00:26:05 -0300

    Aula11-Routes

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,53 +1,24 @@
-import "./App.css";
-import HelloWorld from "./components/HelloWorld";
-import SayMyName from "./components/SayMyName";
-import Pessoa from "./components/Pessoa";
-import List from "./components/List";
-import Evento from "./components/Evento";
-import Form from "./components/Form";
-import Condicional from "./components/Condicional";
-import OutraLista from "./components/OutraLista";
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import Home from './pages/Home';
 
 function App() {
-  const name = "Luis";
-  const newName = name.toUpperCase();
-  const meusItens = ['Reaact', 'Vue', 'Angular']
-
-  function sum(a, b) {
-    return a + b;
-  }
-
-  const url =
-    "https://www.petz.com.br/blog/wp-content/uploads/2021/03/piercing-para-cachorro-2.jpg";
-
   return (
-    <div className="App">
-      <Evento />
-      <Form />
-      <h1>Olá, {newName}</h1>
-      <h1>Soma, {sum(1, 2)}</h1>
-      <img src={url} alt="Minha Imagem" />
-      <HelloWorld />
-      <SayMyName nome="Gustavo" />
-      <SayMyName nome="João" />
-      <SayMyName nome={name} />
-
-      <Pessoa
-        nome="Caule"
-        idade="12"
-        profissao="estudante"
-        foto="https://fw.atarde.uol.com.br/2021/11/750_jovem-exame-pm_20211162093361.jpg"
-      />
+    <Router>
+      <Navbar />
       
-      <List />
-      <br />
-      <Condicional />
+      <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/empresa" element={<Empresa/>}></Route>
+      <Route path="/contato" element={<Contato/>}></Route>
+      </Routes>
 
-      <h1>Renderização de Listas</h1>
-      <OutraLista itens={meusItens} />
-
-    </div>
-  );
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
--- first-project/src/components/SayMyName.js
+++
@@ -1,9 +0,0 @@
-function SayMyName(propiedade){
-    return(
-        <div>
-            <p>Fala aí {propiedade.nome}, suave</p>
-        </div>
-    )
-}
-
-export default SayMyName
