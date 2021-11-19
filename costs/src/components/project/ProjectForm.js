af5b3cc269bd44f31ac9cbc0a055e869b38e71ae
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-11-19 00:08:20 -0300

    ProjetoFinal_04_ConectandoAPI

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,31 +1,51 @@
-import Input from '../form/Input'
-import Select from '../form/Select'
-import SubmitButton from '../form/SubmitButton'
-import styles from './ProjectForm.module.css'
+import { useState, useEffect } from "react";
 
-function ProjectForm({btnText}){
-    return(
-        <form className={styles.form}>
-            <Input 
-            type="text"
-            text="Nome do Projeto"
-            name="name"
-            placeholder="Insira o nome do projeto"
-             />
-            <Input 
-            type="number"
-            text="Orçamento do Projeto"
-            name="name"
-            placeholder="Insira o orçamento do projeto"
-             />
-            <div>
-                <Select name="category_id" text="Selecione a categoria" />
-            </div>
-            <div>
-                <SubmitButton text={btnText}/>
-            </div>
-        </form>
-    )
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+import styles from "./ProjectForm.module.css";
+
+function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categorias", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do Projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento do Projeto"
+        name="name"
+        placeholder="Insira o orçamento do projeto"
+      />
+
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
+
+      <SubmitButton text={btnText} />
+    </form>
+  );
 }
 
-export default ProjectForm
+export default ProjectForm;
