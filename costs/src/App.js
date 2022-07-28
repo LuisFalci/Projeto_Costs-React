78d20b9e856bbc915351c82893ace7af5411744c
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2022-07-27 23:54:13 -0300

    Update App.js

--- costs/src/App.js
+++ costs/src/App.js
@@ -20,6 +20,7 @@ function App() {
 
 
 
+
           <Route path="/company" element={<Company />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/newproject" element={<NewProject />}></Route>
